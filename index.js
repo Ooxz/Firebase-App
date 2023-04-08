51161504334ae965b4fe6227dbddfabfc4ef522e
Brune <abrune.osteo@gmail.com>
2023-04-08 23:23:26 +0200

    update firebase

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
 const appSettings = {
-    databaseURL: "https://shoppingcart-493a1-default-rtdb.europe-west1.firebasedatabase.app/"
+    databaseURL: "https://realtime-database-c0163-default-rtdb.europe-west1.firebasedatabase.app/"
 }
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
 
 const addBtn = document.getElementById("add-button")
 const inputField = document.getElementById("input-field")
+const shoppingList = document.getElementById("shopping-list")
 
 addBtn.addEventListener("click", inputFieldValue)
 
+
+const clearInputField = () => {
+	inputField.value = ""
+}
+
+const shoppingListInnerHTML = (inputValue) => {
+	shoppingList.innerHTML += `<li>${inputValue}</li>`
+}
+
 function inputFieldValue() {
-	console.log(inputField.value)
+	let inputValue = inputField.value
+	clearInputField()
+	push(shoppingListInDB, inputValue)
+	shoppingListInnerHTML(inputValue)
+	
+	
 }
